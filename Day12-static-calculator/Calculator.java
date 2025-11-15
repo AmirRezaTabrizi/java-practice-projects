8816c6575429fa4c7e22f52e047c7a8a70b34ae6
Amir Reza Tabrizi <amirrep97@gmail.com>
2025-11-15 10:29:38 +0330

    Add Day 12 - Calculator class with static methods

---
+++ Day12-static-calculator/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static int mul(int a, int b) {
+        return a * b;
+    }
+
+    public static double div(double a, double b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero!");
+            return 0;
+        }
+        return a / b;
+    }
+}

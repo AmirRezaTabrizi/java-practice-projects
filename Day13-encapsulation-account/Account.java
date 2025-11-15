f6d5cd041e76bb4f332e24df512dfb11200abc12
Amir Reza Tabrizi <amirrep97@gmail.com>
2025-11-15 10:33:51 +0330

    Add Day 13 - Account class with encapsulation and getters/setters

---
+++ Day13-encapsulation-account/Account.java
@@ -0,0 +1,17 @@
+public class Account {
+    private double balance;
+
+    public Account(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+}
